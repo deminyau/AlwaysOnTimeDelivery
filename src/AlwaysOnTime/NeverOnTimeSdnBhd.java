ce3bd9e9d5af3a3024b2c5bebd12ac98807da091
deminyau <74088997+deminyau@users.noreply.github.com>
2021-05-21 09:29:18 +0800

    ydm
    try catch

--- src/AlwaysOnTime/NeverOnTimeSdnBhd.java
+++ src/AlwaysOnTime/NeverOnTimeSdnBhd.java
@@ -17,65 +17,69 @@ public class NeverOnTimeSdnBhd {
         System.out.println("*********************************");
         System.out.println("------Never On Time Sdn Bhd------");
         System.out.println("*********************************");
-        System.out.print("Enter file: ");
-        String filename = group123.nextLine();
-        System.out.println("*********************************");
-        message1();
-        int choice;
-        //System.out.print("Enter choice to be proceed: ");
-        do {
+        try {
+            System.out.print("Enter file: ");
+            String filename = group123.nextLine();
+            System.out.println("*********************************");
+            message1();
+            int choice;
+            //System.out.print("Enter choice to be proceed: ");
+            do {
 
-            System.out.print("Enter choice to be proceed: ");
-            choice = group123.nextInt();
-            System.out.println("-----------------------------");
+                System.out.print("Enter choice to be proceed: ");
+                choice = group123.nextInt();
+                System.out.println("-----------------------------");
+                System.out.println("");
+
+                if (choice == 1) {
+
+                    BasicBFS bfs = new BasicBFS(filename);
+                    System.out.println("-----------------------------");
+
+                } else if (choice == 2) {
+
+                    Greedy g = new Greedy(filename);
+                    g.GreedySimulation();
+                    System.out.println("-----------------------------");
+
+                } else if (choice == 3) {
+
+                    //MCTS mcts = new MCTS(filename);
+                    //mcts.MCTS_Simulation;
+                    System.out.println("-----------------------------");
+
+                } else if (choice == 4) {
+
+                    A_Star as = new A_Star(filename);
+                    as.AStarSimulation();
+                    System.out.println("-----------------------------");
+
+                } else if (choice == 5) {
+
+                    Best_First bf = new Best_First(filename);
+                    bf.BFirstSimulation();
+                    System.out.println("-----------------------------");
+
+                } else if (choice == 6) {
+
+                    ExtraAlgo ea = new ExtraAlgo(filename);
+                    ea.ExtraAlgo();
+                    System.out.println("-----------------------------");
+
+                } else if (choice == 7) {
+
+                    SDHC sdhc = new SDHC(filename);
+                    sdhc.SDHCSimulation();
+                    System.out.println("-----------------------------");
+
+                } else {
+                }
+            } while (choice != 8);
             System.out.println("");
-
-            if (choice == 1) {
-
-                BasicBFS bfs = new BasicBFS(filename);
-                System.out.println("-----------------------------");
-
-            } else if (choice == 2) {
-
-                Greedy g = new Greedy(filename);
-                g.GreedySimulation();
-                System.out.println("-----------------------------");
-
-            } else if (choice == 3) {
-
-                //MCTS mcts = new MCTS(filename);
-                //mcts.MCTS_Simulation;
-                System.out.println("-----------------------------");
-
-            } else if (choice == 4) {
-
-                A_Star as = new A_Star(filename);
-                as.AStarSimulation();
-                System.out.println("-----------------------------");
-
-            } else if (choice == 5) {
-
-                Best_First bf = new Best_First(filename);
-                bf.BFirstSimulation();
-                System.out.println("-----------------------------");
-
-            } else if (choice == 6) {
-
-                ExtraAlgo ea = new ExtraAlgo(filename);
-                ea.ExtraAlgo();
-                System.out.println("-----------------------------");
-
-            } else if (choice == 7) {
-
-                SDHC sdhc = new SDHC(filename);
-                sdhc.SDHCSimulation();
-                System.out.println("-----------------------------");
-
-            } else {
-            }
-        } while (choice != 8);
-        System.out.println("");
-        System.out.println("Exit successfully");
+            System.out.println("Exit successfully");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
     }
 
     public static void message1() {
